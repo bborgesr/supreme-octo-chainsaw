4531283fe27a34e45fa4d5a3adb66bf7c7eda079
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-02-20 17:15:33 +0000

    first commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from "@angular/core";
+
+import { Book } from "app/models/book";
+import { allBooks, allReaders } from "app/data";
+import { Reader } from "app/models/reader";
+import { LoggerService } from "app/services/logger.service";
+
+@Component({
+  selector: "app-dashboard",
+  templateUrl: "./dashboard.component.html",
+  styles: []
+})
+export class DashboardComponent implements OnInit {
+  allBooks: Book[] = allBooks;
+  allReaders: Reader[] = allReaders;
+  mostPopularBook: Book = allBooks[0];
+
+  constructor(private loggerService: LoggerService) {
+    this.loggerService.log("Creating the dashboard");
+  }
+
+  ngOnInit() {}
+
+  deleteBook(bookID: number): void {
+    console.warn(`Delete book not yet implemented (bookID: ${bookID}).`);
+  }
+
+  deleteReader(readerID: number): void {
+    console.warn(`Delete reader not yet implemented (readerID: ${readerID}).`);
+  }
+}
